20c0af93ffb59d291a115fbac1bc84542bd1f99e
SwatOfSky <dimadnper1997@gmail.com>
2022-06-18 12:34:57 +0300

    routes_added

---
+++ src/components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './News.module.css';
+
+const News = () => {
+    return (
+        <div className='app-wrapper-content'>
+            News
+        </div>
+    )
+}
+
+export default News
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 import s from './Profile.module.css'
 
 const Profile = () => {
-  return <div className={s.content}>
+  return <div className='app-wrapper-content'>
     <div>
       <img src='https://cdn.pixabay.com/photo/2019/11/07/08/12/sea-4608198_960_720.jpg' />
     </div>
